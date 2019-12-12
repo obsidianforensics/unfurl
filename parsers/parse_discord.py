0ff373f675278663058d70d43c8b397cc403e3d5
Ryan <ryan@obsidianforensics.com>
2019-12-12 11:40:32 -0800

    Add parsing of Discord attached files.

--- parsers/parse_discord.py
+++ parsers/parse_discord.py
@@ -38,7 +38,7 @@ def parse_discord_snowflake(unfurl, node):
                  '<a href="https://discordapp.com/developers/docs/reference#snowflakes" target="_blank">[ref]</a>'
 
     unfurl.add_to_queue(
-        data_type='epoch-milliseconds', key=None, value=timestamp, label='Timestamp: {}'.format(timestamp),
+        data_type='epoch-milliseconds', key=None, value=timestamp, label='Timestamp:\n{}'.format(timestamp),
         hover='The first value in a Discord Snowflake is a timestamp associated with object creation',
         parent_id=node.node_id, incoming_edge_config=discord_edge)
 
@@ -60,8 +60,9 @@ def parse_discord_snowflake(unfurl, node):
 
 def run(unfurl, node):
 
-    # Known patterns from main discordapp.com site
-    if 'discordapp.com' in unfurl.find_preceding_domain(node):
+    # Known patterns from main Discord site
+    discord_domains = ['discordapp.com', 'discordapp.net']
+    if any(discord_domain in unfurl.find_preceding_domain(node) for discord_domain in discord_domains):
         if node.data_type == 'url.path.segment':
             # Viewing a channel on a server
             # Ex: https://discordapp.com/channels/427876741990711298/551531058039095296
@@ -71,11 +72,16 @@ def run(unfurl, node):
                         data_type='description', key=None, value=None, label='Server ID',
                         hover='The timestamp in the associated Discord Snowflake is the time of Server creation',
                         parent_id=node.node_id, incoming_edge_config=discord_edge)
+                    parse_discord_snowflake(unfurl, node)
+
                 elif node.key == 3:
                     unfurl.add_to_queue(
                         data_type='description', key=None, value=None, label='Channel ID',
-                        hover='The timestamp in the associated Discord Snowflake is the time of Channel creation on the given Server',
+                        hover='The timestamp in the associated Discord Snowflake is the time of Channel creation '
+                              'on the given Server',
                         parent_id=node.node_id, incoming_edge_config=discord_edge)
+                    parse_discord_snowflake(unfurl, node)
+
                 # Linking to a specific message
                 # Ex: https://discordapp.com/channels/427876741990711298/537760691302563843/643183730227281931
                 elif node.key == 4:
@@ -83,11 +89,30 @@ def run(unfurl, node):
                         data_type='description', key=None, value=None, label='Message ID',
                         hover='The timestamp in the associated Discord Snowflake is the time the Message was sent',
                         parent_id=node.node_id, incoming_edge_config=discord_edge)
-                parse_discord_snowflake(unfurl, node)
+                    parse_discord_snowflake(unfurl, node)
 
-    # TODO: Parse attachment URLs
-    # Ex: https://cdn.discordapp.com/attachments/622136585277931532/626893414490832918/Discord_Developer_Portal_Snowflakes.pdf
-    #              ^                     ^        ^ uploader ID?      ^ file id, with timestamp of upload     ^ filename
+            # File Attachment URLs
+            # Ex: https://cdn.discordapp.com/attachments/622136585277931532/626893414490832918/asdf.png
+            elif unfurl.check_sibling_nodes(node, data_type='url.path.segment', key=1, value='attachments'):
+                if node.key == 2:
+                    unfurl.add_to_queue(
+                        data_type='description', key=None, value=None, label='Channel ID',
+                        hover='The timestamp in the associated Discord Snowflake is the time of channel creation',
+                        parent_id=node.node_id, incoming_edge_config=discord_edge)
+                    parse_discord_snowflake(unfurl, node)
+
+                elif node.key == 3:
+                    unfurl.add_to_queue(
+                        data_type='description', key=None, value=None, label='File ID',
+                        hover='The timestamp in the associated Discord Snowflake is the time the attachment '
+                              'was uploaded.',
+                        parent_id=node.node_id, incoming_edge_config=discord_edge)
+                    parse_discord_snowflake(unfurl, node)
+
+                elif node.key == 4:
+                    unfurl.add_to_queue(
+                        data_type='description', key=None, value=None, label='Attachment File Name',
+                        parent_id=node.node_id, incoming_edge_config=discord_edge)
 
     # Check if the node's value would correspond to a Snowflake with timestamp between 2015-02 and 2020-04
     elif unfurl.check_if_int_between(node.value, 15000000000000000, 700000000000000000):
