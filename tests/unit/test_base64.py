9d5d2d0ec2748578d7151911f6aac4acb1335dcc
Ryan Benson <ryan@obsidianforensics.com>
2020-01-22 14:14:13 -0800

    Add simple base64 tests

---
+++ tests/unit/test_base64.py
@@ -0,0 +1,79 @@
+from unfurl import Unfurl
+
+import unittest
+
+
+class TestBase64(unittest.TestCase):
+
+    def test_padded_b64_ascii(self):
+        """ Test a simple ASCII string that is base64-encoded."""
+
+        test = Unfurl()
+        test.add_to_queue(
+            data_type='url', key=None,
+            value='dGVzdHl0ZXN0dGVzdA==')
+        test.parse_queue()
+
+        # check the number of nodes
+        self.assertEqual(len(test.nodes.keys()), 2)
+        self.assertEqual(test.total_nodes, 2)
+
+        # confirm that it was detected as b64
+        self.assertEqual('b64', test.nodes[2].data_type)
+
+        # confirm that text decoded correctly
+        self.assertEqual('testytesttest', test.nodes[2].value)
+
+        # make sure the queue finished empty
+        self.assertTrue(test.queue.empty())
+        self.assertEqual(len(test.edges), 0)
+
+    def test_unpadded_b64_ascii(self):
+        """ Test a simple ASCII string that is base64-encoded, with padding removed."""
+
+        test = Unfurl()
+        test.add_to_queue(
+            data_type='url', key=None,
+            value='dGVzdHl0ZXN0dGVzdA')
+        test.parse_queue()
+
+        # check the number of nodes
+        self.assertEqual(len(test.nodes.keys()), 2)
+        self.assertEqual(test.total_nodes, 2)
+
+        # confirm that it was detected as b64
+        self.assertEqual('b64', test.nodes[2].data_type)
+
+        # confirm that text decoded correctly
+        self.assertEqual('testytesttest', test.nodes[2].value)
+
+        # make sure the queue finished empty
+        self.assertTrue(test.queue.empty())
+        self.assertEqual(len(test.edges), 0)
+
+    def test_incorrect_padded_b64_ascii(self):
+        """ Test a simple ASCII string that is base64-encoded, with incorrect padding"""
+
+        test = Unfurl()
+        test.add_to_queue(
+            data_type='url', key=None,
+            value='dGVzdHl0ZXN0dGVzdA=')
+        test.parse_queue()
+
+        # check the number of nodes
+        self.assertEqual(len(test.nodes.keys()), 2)
+        self.assertEqual(test.total_nodes, 2)
+
+        # confirm that it was detected as b64
+        self.assertEqual('b64', test.nodes[2].data_type)
+
+        # confirm that text decoded correctly
+        self.assertEqual('testytesttest', test.nodes[2].value)
+
+        # make sure the queue finished empty
+        self.assertTrue(test.queue.empty())
+        self.assertEqual(len(test.edges), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
