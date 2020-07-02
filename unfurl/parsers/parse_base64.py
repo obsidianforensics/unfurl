9bc3e9d6743861b87824f47af5cc3a5036399e3b
Ryan Benson <ryan@obsidianforensics.com>
2020-07-02 16:58:25 -0700

    Improvements to b64 handling (better filters before trying it)

--- unfurl/parsers/parse_base64.py
+++ unfurl/parsers/parse_base64.py
@@ -20,24 +20,37 @@ def run(unfurl, node):
         # A valid b64 string will not be this length
         return False
 
-    # TODO: better validation/detection/etc. This is very basic.
-    m = re.match(r'^[A-Za-z0-9_=\-]{16,}$', node.value)
-    if m:
+    urlsafe_b64_m = re.fullmatch(r'[A-Za-z0-9_=\-]{8,}', node.value)
+    standard_b64_m = re.fullmatch(r'[A-Za-z0-9+/=]{8,}', node.value)
+    long_int = re.fullmatch(r'\d{8,}', node.value)
+
+    # Long integers pass the b64 regex, but we don't want those here.
+    if long_int:
+        return
+
+    decoded = None
+    if urlsafe_b64_m:
         decoded = base64.urlsafe_b64decode(unfurl.add_b64_padding(node.value))
-        if decoded == node.value:
-            return
-        try:
-            # This limits the plugin to only decoding ASCII string that were base64 encoded. Obviously other
-            # things could be encoded, but it's a start.
-            str_decoded = decoded.decode('ascii', errors='strict')
+    elif standard_b64_m:
+        decoded = base64.b64decode(unfurl.add_b64_padding(node.value))
 
-        # This will happen a lot with things that aren't really b64 encoded, or with things that are b64-encoded,
-        # but the results are not ASCII (like gzip or protobufs).
-        except UnicodeDecodeError:
-            # Show the resulting bytes from base64 inflating. Disabled for now, as it's too noisy.
-            # unfurl.add_to_queue(data_type='bytes', key=None, value=decoded,
-            #                     parent_id=node.node_id, incoming_edge_config=b64_edge)
-            return
+    if decoded == node.value or not decoded:
+        return
 
-        unfurl.add_to_queue(data_type='b64', key=None, value=str_decoded,
-                            parent_id=node.node_id, incoming_edge_config=b64_edge)
+    try:
+        # This limits the plugin to only decoding ASCII string that were base64
+        # encoded. Obviously other things could be encoded, but it's a start.
+        str_decoded = decoded.decode('ascii', errors='strict')
+
+    # This will happen a lot with things that aren't really b64 encoded, or
+    # with things that are b64-encoded, but the results are not ASCII
+    # (like gzip or protobufs).
+    except UnicodeDecodeError:
+        # Show the resulting bytes from base64 inflating. Disabled for now,
+        # as it's too noisy.
+        # unfurl.add_to_queue(data_type='bytes', key=None, value=decoded,
+        #                parent_id=node.node_id, incoming_edge_config=b64_edge)
+        return
+
+    unfurl.add_to_queue(data_type='b64', key=None, value=str_decoded,
+                        parent_id=node.node_id, incoming_edge_config=b64_edge)
