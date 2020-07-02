bebb64a60c38377f8a2391ba8e6346ff52745a28
Ryan Benson <ryan@obsidianforensics.com>
2020-07-02 16:20:01 -0700

    Add support for reading file as input and writing CSV file output.

--- unfurl_cli.py
+++ unfurl_cli.py
@@ -15,6 +15,8 @@
 # limitations under the License.
 
 import argparse
+import csv
+import os
 from unfurl import unfurl
 
 
@@ -25,24 +27,57 @@ def main():
     parser.add_argument(
         'what_to_unfurl',
         help='what to unfurl. typically this is a URL, but it also supports integers (timestamps), '
-             'encoded protobufs, and more.')
+             'encoded protobufs, and more. if this is instead a file path, unfurl will open '
+             'that file and process each line in it as a URL.')
     parser.add_argument(
         '-d', '--detailed', help='show more detailed explanations.', action='store_true')
     parser.add_argument(
-        '-f', '--filter',
-        help='only output lines that match this filter.')
+        '-f', '--filter', help='only output lines that match this filter.')
+    parser.add_argument(
+        '-o', '--output',
+        help='file to save output (as CSV) to. if omitted, output is sent to '
+             'stdout (typically this means displayed in the console).')
     parser.add_argument(
         '-v', '-V', '--version', action='version', version='unfurl v20200613')
     args = parser.parse_args()
 
-    unfurl_instance = unfurl.Unfurl()
-    unfurl_instance.add_to_queue(
-        data_type='url', key=None,
-        value=args.what_to_unfurl)
-    unfurl_instance.parse_queue()
+    items_to_unfurl = []
 
-    print(unfurl_instance.generate_text_tree(
-        detailed=args.detailed, output_filter=args.filter))
+    if os.path.isfile(args.what_to_unfurl):
+        with open(args.what_to_unfurl) as f:
+            for input_url in f:
+                items_to_unfurl.append(input_url)
+
+    else:
+        items_to_unfurl.append(args.what_to_unfurl)
+
+    if args.output:
+        with open(args.output, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+            csv_writer.writerow(['url', 'unfurled'])
+
+            for item in items_to_unfurl:
+                unfurl_instance = unfurl.Unfurl()
+                unfurl_instance.add_to_queue(
+                    data_type='url', key=None,
+                    value=item)
+                unfurl_instance.parse_queue()
+                csv_writer.writerow(
+                    [item, unfurl_instance.generate_text_tree(
+                        detailed=args.detailed,
+                        output_filter=args.filter)])
+
+    else:
+        for item in items_to_unfurl:
+            unfurl_instance = unfurl.Unfurl()
+            unfurl_instance.add_to_queue(
+                data_type='url', key=None,
+                value=item)
+            unfurl_instance.parse_queue()
+
+            print(unfurl_instance.generate_text_tree(
+                detailed=args.detailed, output_filter=args.filter))
+            print()
 
 
 if __name__ == "__main__":
