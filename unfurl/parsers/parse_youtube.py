4247fa7798d3de98a10e08189a93484a2bfe8eea
kistee <christixd@safe-mail.net>
2020-08-02 22:39:35 +0200

    Fix requested changes

--- unfurl/parsers/parse_youtube.py
+++ unfurl/parsers/parse_youtube.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 
 
+import time
+
+
 youtube_edge = {
     'color': {
         'color': '#ff0000'
@@ -22,11 +25,32 @@ youtube_edge = {
 }
 
 
+def format_seconds(seconds):
+    if seconds < 60:
+        return f"{seconds} seconds"
+    elif seconds < 3600:
+        return time.strftime("%M:%S", time.gmtime(seconds)) + " minutes"
+    else:
+        return time.strftime("%H:%M:%S", time.gmtime(seconds)) + " hours"
+
+
 def run(unfurl, node):
     youtube_domains = ['youtube.com', 'youtu.be']
     if any(youtube_domain in unfurl.find_preceding_domain(node) for youtube_domain in youtube_domains):
-        if node.key == 't':
-            param_text = f'The youtube video will begin playing at {str(node.value)} seconds.'
+        if node.key == 't' or node.key == 'time_continue' or node.key == 'start':
+            try:
+                time_formatted = format_seconds(int(node.value))
+                param_text = f'The youtube video will begin playing at {time_formatted}.'
+                unfurl.add_to_queue(
+                    data_type='descriptor', key=None, value=node.value, label=param_text,
+                    parent_id=node.node_id, incoming_edge_config=youtube_edge)
+            except:
+                pass
+        if node.key == 'v' or (node.data_type == 'url.path.segment' and (not 'watch' in node.value and not 'embed' in node.value)) or (node.data_type == 'url.path' and len(node.value[1:].split('/')) == 1 and not '/watch' in node.value and not '/embed' in node.value):
+            video_id = node.value
+            if node.data_type == 'url.path':
+                video_id = video_id[1:]
+            param_text = f'Video ID: {video_id}'
             unfurl.add_to_queue(
                 data_type='descriptor', key=None, value=node.value, label=param_text,
                 parent_id=node.node_id, incoming_edge_config=youtube_edge)
