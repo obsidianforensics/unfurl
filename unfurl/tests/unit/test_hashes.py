fc697e2f89f7e0fab8ce720ca56b1ba66a8c5af7
Ryan <ryan@obsidianforensics.com>
2022-10-24 15:22:15 -0700

    Removing the hash plaintext lookup test, as the site is down/flaky.

--- unfurl/tests/unit/test_hashes.py
+++ unfurl/tests/unit/test_hashes.py
@@ -20,29 +20,6 @@ class TestHashDetection(unittest.TestCase):
         # confirm that detected as MD5
         self.assertIn('Potential MD5 hash', test.nodes[9].label)
 
-    def test_md5_hash_with_lookups(self):
-        """ Test detecting a MD5 in a URL with lookups enabled """
-
-        test = Unfurl(remote_lookups=True)
-        test.add_to_queue(
-            data_type='url', key=None,
-            value='http://test-hashes.com/test?1=5f4dcc3b5aa765d61d8327deb882cf99')
-        test.parse_queue()
-
-        # check the number of nodes
-        self.assertEqual(len(test.nodes.keys()), 10)
-        self.assertEqual(test.total_nodes, 10)
-
-        # confirm that detected as MD5
-        self.assertIn('Potential MD5 hash', test.nodes[9].label)
-
-        # confirm that plaintext lookup succeeded
-        self.assertIn('Plaintext: password', test.nodes[10].label)
-
-        # make sure the queue finished empty
-        self.assertTrue(test.queue.empty())
-        self.assertEqual(len(test.edges), 0)
-
 
 if __name__ == '__main__':
     unittest.main()
