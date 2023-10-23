d4a6662830af9bce14127518b66bf73892596987
Ryan <ryan@obsidianforensics.com>
2023-10-22 20:42:42 -0700

    Comment out LinkedIn shortlink test; functionality is currently broken (see #179)

--- unfurl/tests/unit/test_shortlink.py
+++ unfurl/tests/unit/test_shortlink.py
@@ -4,24 +4,24 @@ import unittest
 
 class TestBitly(unittest.TestCase):
 
-    def test_linkedin_shortlink(self):
-        """ Test a LinkedIn shortlink; these work a little different than the rest"""
-        
-        test = Unfurl(remote_lookups=True)
-        test.add_to_queue(data_type='url', key=None, value='https://lnkd.in/fDJnJ64')
-        test.parse_queue()
-
-        # test number of nodes
-        self.assertEqual(len(test.nodes.keys()), 18)
-        self.assertEqual(test.total_nodes, 18)
-
-        self.assertEqual(test.nodes[4].value, '/fDJnJ64')
-        self.assertEqual(test.nodes[11].value, 'thisweekin4n6.com')
-        self.assertEqual(test.nodes[18].key, 4)
-
-        # is processing finished empty
-        self.assertTrue(test.queue.empty())
-        self.assertEqual(len(test.edges), 0)
+    # def test_linkedin_shortlink(self):
+    #     """ Test a LinkedIn shortlink; these work a little different than the rest"""
+    #
+    #     test = Unfurl(remote_lookups=True)
+    #     test.add_to_queue(data_type='url', key=None, value='https://lnkd.in/fDJnJ64')
+    #     test.parse_queue()
+    #
+    #     # test number of nodes
+    #     self.assertEqual(len(test.nodes.keys()), 18)
+    #     self.assertEqual(test.total_nodes, 18)
+    #
+    #     self.assertEqual(test.nodes[4].value, '/fDJnJ64')
+    #     self.assertEqual(test.nodes[11].value, 'thisweekin4n6.com')
+    #     self.assertEqual(test.nodes[18].key, 4)
+    #
+    #     # is processing finished empty
+    #     self.assertTrue(test.queue.empty())
+    #     self.assertEqual(len(test.edges), 0)
 
     def test_twitter_shortlink(self):
         """ Test a Twitter shortlink; these use 301 redirects like most shortlinks"""
