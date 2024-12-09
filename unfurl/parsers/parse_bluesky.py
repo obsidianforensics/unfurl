6f6eecf94a447c4340464c7d6eaaf6daea8382bb
Ryan <ryan@obsidianforensics.com>
2024-12-08 21:36:59 -0800

    Add parsing/lookup of bsky handle->`did`, and lookup `did` creation in audit log.

--- unfurl/parsers/parse_bluesky.py
+++ unfurl/parsers/parse_bluesky.py
@@ -13,9 +13,14 @@
 # limitations under the License.
 
 import base64
+from typing import Union
 import re
+import requests
 
 import logging
+
+import unfurl.app
+
 log = logging.getLogger(__name__)
 
 bluesky_edge = {
@@ -26,6 +31,14 @@ bluesky_edge = {
     'label': '🦋'
 }
 
+atproto_edge = {
+    'color': {
+        'color': '#3b82f6'
+    },
+    'title': 'ATProtocol',
+    'label': '@atproto'
+}
+
 tid_re = re.compile(r'[2-7a-z]{13}')
 
 # Create a mapping from "base32-sortable" alphabet to standard base32 alphabet
@@ -33,7 +46,8 @@ BASE32_SORTABLE_ALPHABET = "234567abcdefghijklmnopqrstuvwxyz"
 STANDARD_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 BASE32_SORTABLE_TRANS = str.maketrans(BASE32_SORTABLE_ALPHABET, STANDARD_ALPHABET)
 
-def parse_bluesky_tid(unfurl, node):
+
+def parse_bluesky_tid(unfurl: unfurl.core.Unfurl, node: unfurl.core.Unfurl.Node) -> None:
     # Ref: https://atproto.com/specs/record-key#record-key-type-tid
     assert tid_re.fullmatch(node.value), "Bluesky TID is not in the expected format (base32-sortable)"
     assert not ord(node.value[0]) & 0x40, "Bluesky TID high bit is set; it must be 0"
@@ -53,14 +67,75 @@ def parse_bluesky_tid(unfurl, node):
         parent_id=node.node_id, incoming_edge_config=bluesky_edge)
 
 
-def run(unfurl, node):
-    if isinstance(node.value, str) and re.fullmatch(tid_re, node.value):
-        if node.data_type == 'url.path.segment':
-            preceding_domain = unfurl.find_preceding_domain(node)
-            if preceding_domain in ['bsky.app']:
-                parse_bluesky_tid(unfurl, node)
+def resolve_bsky_handle_to_did(unfurl: unfurl.core.Unfurl, handle: str) -> str | None:
+    if not unfurl.remote_lookups:
+        return None
+    r = requests.get(f'https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle={handle}')
+    if r.status_code == 200 and r.content and r.json().get('did'):
+        return r.json()['did']
+    else:
+        return None
 
-        # If it's the "root" node and in the format of a TID, parse it.
-        # This case covers someone parsing just an ID, not a full URL.
-        elif node.node_id == 1:
+
+def add_resolved_did_node(unfurl: unfurl.core.Unfurl, node: unfurl.core.Unfurl.Node, did: str) -> None:
+    unfurl.add_to_queue(
+        data_type='did.plc', key='did:plc', value=did.split(':', 2)[2],
+        label=f'Bluesky handle resolves to {did}',
+        # https://atproto.com/specs/did and https://github.com/did-method-plc/did-method-plc
+        hover='Bluesky uses Decentralized identifiers ("DIDs") as persistent, long-term identifiers '
+              'for every account.',
+        parent_id=node.node_id, incoming_edge_config=bluesky_edge)
+
+
+def get_did_plc_audit_log_values(unfurl: unfurl.core.Unfurl, did: str, record_index: int = None, field: str = None) -> Union[str, dict, False]:
+    if not unfurl.remote_lookups:
+        return False
+    r = requests.get(f'https://plc.directory/{did}/log/audit')
+    if r.status_code == 200:
+        if record_index is not None and field:
+            return r.json()[record_index][field]
+        else:
+            return r.json()
+
+
+def run(unfurl: unfurl.core.Unfurl, node: unfurl.core.Unfurl.Node) -> None:
+    if not isinstance(node.value, str):
+        return
+
+    # We can use the plc.directory audit logs to find when a DID was created. We'll do this
+    # on both nodes that were created by Unfurl from Bluesky handle lookups (which sets the
+    # data_type to 'did.plc', or if we encounter a URL path segment or other node that looks
+    # to be a did:plc string.
+    if node.data_type == 'did.plc' or node.value.startswith('did:plc'):
+        did_plc_value = node.value
+        if node.data_type == 'did.plc':
+            did_plc_value = f'did:plc:{node.value}'
+        created = get_did_plc_audit_log_values(unfurl, did_plc_value, record_index=0, field='createdAt')
+        if created and isinstance(created, str):
+            unfurl.add_to_queue(
+                data_type='timestamp.is08601', key='createdAt', value=created, label=f'Created at {created}',
+                hover='The audit log for DIDs on plc.directory contains "createdAt" timestamps.',
+                parent_id=node.node_id, incoming_edge_config=bluesky_edge)
+
+    # On bsky.app, handles have a predictable location in URLs. If a URL starts with https://bsky.app/profile,
+    # the next URL path segment should be a Bluesky "handle" (or DID). We can resolve this handles to the backing
+    # decentralized identifier (DID) using a Bluesky API. We can then do more lookups using that DID.
+    if node.data_type == 'url.path.segment':
+        preceding_domain = unfurl.find_preceding_domain(node)
+        if preceding_domain in ['bsky.app']:
+            if re.fullmatch(tid_re, node.value):
+                parse_bluesky_tid(unfurl, node)
+            elif node.key == 2 and unfurl.check_sibling_nodes(node, data_type='url.path.segment', key=1, value='profile'):
+                did = resolve_bsky_handle_to_did(unfurl, node.value)
+                if did and isinstance(did, str):
+                    add_resolved_did_node(unfurl, node, did)
+
+    # If it's the "root" node and in a format we recognize related to Bluesky, parse it.
+    # This case covers someone parsing just an ID or handle, not a full URL.
+    elif node.node_id == 1:
+        if re.fullmatch(tid_re, node.value):
             parse_bluesky_tid(unfurl, node)
+        elif node.value.endswith('.bsky.social'):
+            did = resolve_bsky_handle_to_did(unfurl, node.value)
+            if did and isinstance(did, str):
+                add_resolved_did_node(unfurl, node, did)
