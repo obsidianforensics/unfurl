f3f0d02498e2632859d2b3063e122f1e3386a336
Ryan <ryan@obsidianforensics.com>
2025-02-18 07:19:23 -0800

    Tighten b64+proto parsing to filter out all digits or all number paths, including slashes.

--- unfurl/__init__.py
+++ unfurl/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Ryan Benson
+# Copyright 2025 Ryan Benson
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 __author__ = "Ryan Benson"
-__version__ = "20241121"
+__version__ = "20250218"
 __email__ = "ryan@dfir.blog"
 
 import logging
